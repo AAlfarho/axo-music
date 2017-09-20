e7c40b322c73ca981c5d9927ac8c23a70a5b91f1
AAlfarho <st.alfaro@gmail.com>
2017-09-19 21:18:00 -0700

    splash screen css

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,44 +3,37 @@ import {Link} from 'react-router-dom';
 
 const Splash = (props) => (
 <div className="splash">
-  <div id="bg-wrap" className="loaded">
-    <div id="login">
-      <div id="login-method">
-        <div className="step show" id="screen-signup-flow">
-          <div className="step-outer">
-            <div className="step-inner">
-              <div className="heading logo">
-                <img alt="" src="https://open.scdn.co/static/images/logo-white-2x.png"/>
-              </div>
-              <button id="signup-spotify" className="btn-landing btn-green" data-or="or" data-contextual="false">
-                <Link to={props.signup_url}>
-                  Sign Up
-                </Link>
-              </button>
-              <div className="separator-container">
-                <div className="separator-line"></div>
-                <div className="separator-text">¿Ya tienes cuenta?</div>
-                <div className="separator-line">
-                </div>
-              </div>
-              <button id="has-account" className="btn-landing btn-white" data-or="or">
-                <Link to={props.login_url}>
-                  Login
-                </Link>
-              </button>
+  <div className="hero-image">
+    <div className="splash-wrapper">
+      <div className="display-options">
+        <div className="left-most-options">
+          <div className="small-logo">
+            <img alt="" src="https://open.scdn.co/static/images/logo-white-2x.png"/>
+          </div>
+          <button id="signup-spotify" className="btn-xl btn-green" data-or="or" data-contextual="false">
+            <Link to={props.signup_url}>
+              Sign Up
+            </Link>
+          </button>
+          <div className="separator-container">
+            <div className="separator-line"></div>
+            <div className="separator-line">
             </div>
           </div>
+        <button className="btn btn-xl btn-white" data-or="or">
+            <Link to={props.login_url}>
+              Login
+            </Link>
+          </button>
         </div>
-
-        <div id="login-features">
-          <h1>Consigue la música perfecta</h1>
-          <h3>Escucha millones de canciones gratis.</h3>
+        <div className="right-most-options">
+          <h1 className="green-h1">Get the right music, right now</h1>
+          <h3>Listen to millions of songs for free.</h3>
           <ul>
-            <li>Busca y descubre música que te encantará</li>
-            <li>Crea playlists con tu música favorita</li>
+            <li>Search &amp; discover music you'll love</li>
+            <li>Create playlists of your favorite music</li>
           </ul>
         </div>
-
       </div>
     </div>
   </div>
