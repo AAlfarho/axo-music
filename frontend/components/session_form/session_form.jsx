143e0b7f022d8e03a1d25bc36798d1632fc76d65
AAlfarho <st.alfaro@gmail.com>
2017-09-19 23:44:48 -0700

    Styling for login/signup. Resolves #4

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -45,19 +45,42 @@ class SessionForm extends React.Component {
     const signupFlow = this.props.formType !== 'login';
     const submitButtonText = signupFlow ? 'Sign up' : 'Login';
     return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder='username' value={username}
-            onChange={this.handleChange('username')}/>
-          {
-            signupFlow &&
-            <input type="text" placeholder='Email' value={email}
-              onChange={this.handleChange('email')}/>
-          }
-          <input type='password' value={password} onChange={this.handleChange('password')} />
-          <input type="submit" value={submitButtonText}/>
-        </form>
-        {this.renderErrors()}
+      <div className="splash">
+        <div className="hero-image">
+          <div className="session-form-wrapper">
+            <div className="display-options">
+              <div className="left-most-options">
+                <div className="extra-small-logo">
+                  <img alt="" src="https://open.scdn.co/static/images/logo-white-2x.png"/>
+                </div>
+                <div className="separator-container">
+                  <div className="separator-line"></div>
+                  <div className="separator-line"></div>
+                </div>
+                <div className="session-container">
+                  <form className="session-form"onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder='Username' value={username}
+                      onChange={this.handleChange('username')}/>
+                    {
+                      signupFlow &&
+                      <input type="text" placeholder='Email' value={email}
+                        onChange={this.handleChange('email')}/>
+                    }
+                    <input type='password' value={password} onChange={this.handleChange('password')} placeholder="Password"/>
+                    <input className="btn-xl btn-green"type="submit" value={submitButtonText}/>
+                    {
+                      !signupFlow &&
+                      <button className="btn btn-xl btn-white" data-or="or">
+                          Demo Login
+                        </button>
+                    }
+                  </form>
+                  {this.renderErrors()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
