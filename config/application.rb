79b535150808521151ef12e6b690d5de8b7312cb
AAlfarho <st.alfaro@gmail.com>
2017-09-20 00:45:47 -0700

    add assets path

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module AxoMusic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.paths << Rails.root.join("app", "assets", "images")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
