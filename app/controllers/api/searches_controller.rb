ff6f16f46dc725d389c8e99a3092c883aa218482
AAlfarho <st.alfaro@gmail.com>
2017-09-27 23:09:25 -0700

    update search to include album and artists

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,7 +1,13 @@
 class Api::SearchesController < ApplicationController
   def index
     search_tag = params[:search_tag];
-    @songs_found = Song.where("title ILIKE ?", "%#{search_tag}%")
+    @songs_found = Song.where("title ILIKE ?", "%#{search_tag}%");
+    Artist.where('name ILIKE ?', "%#{search_tag}%").each do |artist|
+      @songs_found += artist.songs
+    end
+    Album.where('title ILIKE ?', "%#{search_tag}%").each do |album|
+      @songs_found += album.songs
+    end
     @users_found = User.where('username ILIKE ? and id != ?', "%#{search_tag}%", current_user.id);
   end
 end
