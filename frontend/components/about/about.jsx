a05c6767118300680936031bb7f56b7441b79476
AAlfarho <st.alfaro@gmail.com>
2017-09-29 01:23:46 -0700

    fix typo

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const About = () =>(
     <div className="hbox about-me-flex-container">
         <div className="vbox about-me-icons">
-            <a  arget="_blank" href="https://github.com/AAlfarho/axo-music">
+            <a  target="_blank" href="https://github.com/AAlfarho/axo-music">
                 <i className="fa fa-github-square fa-2x brand-icons-fa"></i>
             </a>
         </div>
         <div className="hbox about-me-icons">
-            <a arget="_blank" href ="https://www.linkedin.com/in/andrés-alfaro-cortés-b98b1b58/" >
+            <a target="_blank" href ="https://www.linkedin.com/in/andrés-alfaro-cortés-b98b1b58/" >
             <i className="fa fa-linkedin-square fa-2x brand-icons-fa"></i>
             </a>
         </div>
